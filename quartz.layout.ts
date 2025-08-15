a612a7004577071aee5595bcbddad436bd3cb35c
Tsuyoshi Kodama <tsuyoshi.kodama@gmail.com>
2025-08-15 14:16:34 +0000

    feat: add mobile-only Table of Contents to default content page layout

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -24,6 +24,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
+    Component.MobileOnly(Component.TableOfContents()),
   ],
   left: [
     Component.PageTitle(),
